5bad3503cef7e7bba7a79f2b3a8ded79600b7820
TiagoPSouza <developer.tiagotech@gmail.com>
2023-07-28 06:43:11 -0300

    agoraVai

---
+++ proverbios/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>header</h2>
+      <h2>conteúdo</h2>
+      <h2>footer</h2>
+    </div>
+  );
+}
+
+export default App;
