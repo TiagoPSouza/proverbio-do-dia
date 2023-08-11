41ca352caff89f3a5737cdc3529fbc28f3f9aa1b
TiagoPSouza <developer.tiagotech@gmail.com>
2023-08-11 05:23:41 -0300

    11/08/2023 as 05h e 23m

--- proverbios/src/components/Header.js
+++ proverbios/src/components/Header.js
@@ -1,9 +1,11 @@
+import { Menu } from './Menu'
 import './styleHeader.css'
 
 export const Header = () => {
   return (
     <header className='containerHeader'>
         <h2>componente header</h2>
+        <Menu />
     </header>
   )
 }
